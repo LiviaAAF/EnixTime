fd6ede4cbbfb0a34daf6bfa4fb09943f4c521cd7
LiviaAAF <feitosalivia23@gmail.com>
2024-08-25 14:53:08 -0300

    apenas testando

---
+++ header.php
@@ -0,0 +1,31 @@
+<h1>
+    <a href="">
+        <img alt="Arcevo Guapoduo" class="logo" src="/image/Amaranthitem.webp" width="40" height="40">
+        <span><a href="index.html">Acervo Guapoduo</a></span>
+    </a>
+</h1>
+    
+<nav class="navbar">
+    <ul>
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="guapoverse.html">Guapoverso</a>
+        </li>
+        <li>
+            <a href="categories.html">Categorias</a>
+        </li>
+        <li class="dropdown">
+            <a href="">Acessar</a>
+
+            <div class="dropdown-menu">
+                <a href="#">Autores</a>
+                <a href="#">Gênero</a>
+            </div>
+        </li>
+        <li>
+            <a href="about.html">Sobre</a>
+        </li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <?php
+        include 'header.php';
+        ?>
+    </header>
+    <section class="profile" id="profile">
+        <div class="box-container">
+          <div class="box">
+            <img src="/image/oz7KiHfY_400x400.jpg" alt="Haikirii" />
+            <h1>Haikirii</h1>
+            <div>Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste</div>
+          </div>
+
+          <div class="box">
+            <img src="/image/MzV8Tm3T_400x400.jpg" alt="Nan" />
+            <h1>Nan</h1>
+            <div>Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste</div>
+          </div>
+  
+          <div class="box">
+            <img src="/image/blank-profile-picture-973460_960_720.webp" alt="Teste" />
+            <h1>Teste</h1>
+            <div>Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste</div>
+          </div>
+  
+          <div class="box">
+            <img src="/image/blank-profile-picture-973460_960_720.webp" alt="Teste" />
+            <h1>Teste</h1>
+            <div>Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste</div>
+          </div>
+  
+          <div class="box">
+            <img src="/image/blank-profile-picture-973460_960_720.webp" alt="Teste" />
+            <h1>Teste</h1>
+            <div>Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste</div>
+          </div>
+  
+          <div class="box">
+            <img src="/image/blank-profile-picture-973460_960_720.webp" alt="Teste" />
+            <h1>Teste</h1>
+            <div>Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste</div>
+          </div>
+        </div>
+      </section>
+</body>
+</html>
